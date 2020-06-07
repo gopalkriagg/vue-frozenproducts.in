3f921a05d640f75e442d148334faae4596ed6e28
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2020-06-07 16:32:36 +0900

    Create Navbar component, set background image

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Frozen Products</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">About Us</b-nav-item>
+          <b-nav-item href="#">Products</b-nav-item>
+          <b-nav-item href="#">Get a Quote</b-nav-item>
+          <b-nav-item href="#">Contact Us</b-nav-item>
+          <b-nav-item href="#">FAQs</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Facebook</b-nav-item>
+          <b-nav-item href="#">Mail</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    msg: String,
+  },
+};
+</script>
