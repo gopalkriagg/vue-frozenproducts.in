8f7d85d6355ac3f7d2f3801bc53c2b90f3c7612f
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2020-06-07 16:47:52 +0900

    Show image

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div id="app">
     <NavBar />
+    <b-container class="bv-example-row">
+      <b-img src="./assets/modern_frozen_products_logo.png" fluid alt="Responsive image"></b-img>
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    NavBar
-  }
-}
+    NavBar,
+  },
+};
 </script>
 
 <style>
-  body {
-    background-image: url("images/background.jpg");
-    background-repeat: no-repeat;
-    background-size:cover;
-    background-attachment: fixed;
-  }
+body {
+  background-image: url("assets/background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
 </style>
