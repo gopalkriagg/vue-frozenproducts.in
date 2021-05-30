816f9a06e2f7e35571d3c876aadd3668faf4c3e4
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2021-05-30 19:00:56 +0900

    Add social media icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,9 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item :active="$page === 'Home'" href="/index.html">Home</b-nav-item>
+          <b-nav-item :active="$page === 'Home'" href="/index.html"
+            >Home</b-nav-item
+          >
           <b-nav-item :active="$page === 'About'" href="/about.html"
             >About Us</b-nav-item
           >
@@ -20,17 +22,33 @@
           <b-nav-item :active="$page === 'ContactUs'" href="/contact.html"
             >Contact Us</b-nav-item
           >
-          <b-nav-item :active="$page === 'FAQs'" href="/faqs.html">FAQs</b-nav-item>
+          <b-nav-item :active="$page === 'FAQs'" href="/faqs.html"
+            >FAQs</b-nav-item
+          >
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="https://www.facebook.com/frozengreenpeas">Facebook</b-nav-item>
-          <b-nav-item href="https://www.indiamart.com/modernfrozenproducts/">IndiaMart</b-nav-item>
-          <b-nav-item href="https://www.linkedin.com/in/bindu-gupta-049969199/">LinkedIn</b-nav-item>
-          <b-nav-item href="https://www.instagram.com/frozengreenpeas/">Instagram</b-nav-item>
-          <b-nav-item href="https://api.whatsapp.com/send?phone=919815411199&text=">Whatsapp</b-nav-item>
-          <b-nav-item href="mailto:ashwini.bindu@gmail.com">Mail</b-nav-item>
+
+          <b-nav-item href="https://www.facebook.com/frozengreenpeas">
+            <b-icon icon="facebook" style="color: white;"></b-icon>
+          </b-nav-item>
+          <b-nav-item href="https://www.linkedin.com/in/bindu-gupta-049969199/">
+            <b-icon icon="linkedin" style="color: white;"></b-icon>
+          </b-nav-item>
+          <b-nav-item href="https://www.instagram.com/frozengreenpeas/">
+            <b-icon icon="instagram" style="color: white;"></b-icon>
+          </b-nav-item>
+          <b-nav-item href="mailto:ashwini.bindu@gmail.com">
+            <b-icon icon="envelope" style="color: white;"></b-icon>
+          </b-nav-item>
+          <b-nav-item href="https://api.whatsapp.com/send?phone=919815411199&text=">
+            <!-- <b-icon icon="whatsapp" style="color: white;"></b-icon> -->
+            Whatsapp
+          </b-nav-item>
+          <b-nav-item href="https://www.indiamart.com/modernfrozenproducts/">
+            IndiaMart
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
