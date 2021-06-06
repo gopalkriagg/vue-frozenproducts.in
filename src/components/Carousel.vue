2beb5452dc18df3b8779080194a96fd3f2e72abf
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2021-06-06 23:05:11 +0900

    Change carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,52 +1,37 @@
 <template>
   <div>
-    <b-carousel :interval="4000" controls indicators>
-      <b-carousel-slide
-        caption="India Mart Trust Seal"
-        text="Received India Mart Trust Seal"
-        img-src="../assets/india_mart_certificate.jpg"
-      />
+    <b-carousel :interval="4000" controls>
+      <b-carousel-slide img-src="../assets/india_mart_certificate.jpg" />
 
-      <b-carousel-slide
-        caption="Bindu Gupta"
-        img-src="../assets/bindu_gupta.jpg"
-      />
+      <b-carousel-slide img-src="../assets/bindu_gupta.jpg" />
 
-      <b-carousel-slide
-        caption="IQF Green Peas"
-        img-src="../assets/frozen_green_peas.jpg"
-      />
+      <b-carousel-slide img-src="../assets/about_bindu.jpg" />
 
-      <b-carousel-slide
-        caption="Frozen Vegetables Collage"
-        img-src="../assets/product_variety.jpg"
-      />
+      <b-carousel-slide img-src="../assets/fresh_vs_frozen.jpg" />
 
-      <b-carousel-slide
-        caption="IQF Sweet Corn"
-        img-src="../assets/frozen_sweet_corn.jpg"
-      />
+      <b-carousel-slide img-src="../assets/frozen_green_peas.jpg" />
 
-      <b-carousel-slide
-        caption="IQF Mix Vegetables"
-        img-src="../assets/frozen_mixed_vegetable.jpg"
-      />
+      <b-carousel-slide img-src="../assets/frozen_sweet_corn.jpg" />
 
-      <b-carousel-slide
-        caption="IQF Baby Corn"
-        img-src="../assets/corn_on_cob.jpg"
-      />
+      <b-carousel-slide img-src="../assets/frozen_mixed_vegetable.jpg" />
 
-      <b-carousel-slide
-        caption="IQF Cut Beans"
-        img-src="../assets/frozen_cut_beans.jpg"
-      />
+      <b-carousel-slide img-src="../assets/frozen_carrot_diced.jpg" />
 
-      <b-carousel-slide
-        caption="Modern Frozen Products"
-        img-src="../assets/modern_frozen_products.jpg"
-      />
+      <b-carousel-slide img-src="../assets/frozen_cut_beans.jpg" />
 
+      <b-carousel-slide img-src="../assets/frozen_cauliflower_florets.jpg" />
+
+      <b-carousel-slide img-src="../assets/frozen_baby_corn.jpg" />
+
+      <b-carousel-slide img-src="../assets/frozen_broccoli_florets.jpg" />
+
+      <b-carousel-slide img-src="../assets/corn_on_cob.jpg" />
+
+      <b-carousel-slide img-src="../assets/modern_frozen_products.jpg" />
+
+      <b-carousel-slide img-src="../assets/guidance_for_repackers.jpg" />
+
+      <b-carousel-slide img-src="../assets/testimonials.jpg" />
     </b-carousel>
   </div>
 </template>
