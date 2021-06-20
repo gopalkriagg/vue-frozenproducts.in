bf4956567aa679a22764228ae3241e464e7466a7
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2021-06-20 16:44:58 +0900

    Fix google analytics

--- src/ga.js
+++ src/ga.js
@@ -1,6 +1,11 @@
-import Vue from "vue";
-import VueGtag from "vue-gtag";
+import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 
 Vue.use(VueGtag, {
-  config: { id: "UA-143082793-1" },
+  config: {
+    id: 'UA-143082793-1',
+    params: {
+      send_page_view: true,
+    },
+  },
 });
